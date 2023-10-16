98df875150997f6ba06cc51781afe4025ef407ae
Amruth <amruhtkrishnaa@gmail.com>
2023-10-16 23:46:09 +0530

    page 2 feature added

--- newsapp/src/components/NewsCompnents.js
+++ newsapp/src/components/NewsCompnents.js
@@ -7,13 +7,40 @@ export default class NewsCompnents extends Component {
     this.state = {
       article: [],
       loading: false,
+      page:1
     };
   }
 
   async componentDidMount(){
-    let data=await fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=93c17c9c19304ea2891fd67cd8af4f24")
+    let data=await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}&page=1&pageSize=20`)
     let parsedData=await data.json()
-    this.setState({article:parsedData.articles})
+    this.setState({article:parsedData.articles,totalResults:parsedData.totalResults})
+  }
+ 
+  prevBtnClick= async ()=>{
+    console.log("prev click")
+   
+    let data=await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}&page=${this.state.page-1}&pageSize=20`)
+    let parsedData=await data.json()
+    this.setState({
+      article:parsedData.articles,
+      page:this.state.page-1
+    })
+
+  }
+   nextBtnClick=async()=>{
+    console.log("nextclick")
+    if(this.state.page+1 > Math.ceil(this.state.totalResults/20)){
+
+    }else{
+      let data=await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}&page=${this.state.page+1}&pageSize=20`)
+      let parsedData=await data.json()
+      this.setState({
+        article:parsedData.articles,
+        page:this.state.page+1
+    })
+    }
+    
   }
   render() {
     return (
@@ -36,6 +63,10 @@ export default class NewsCompnents extends Component {
               })}
             </div>
           </div>
+        </div>
+        <div className="d-flex justify-content-between">
+        <button type="button" className="btn btn-primary mx-5 " disabled={this.state.page<=1} onClick={this.prevBtnClick}>&larr; Prev</button>
+        <button type="button" className="btn btn-primary mx-5 " onClick={this.nextBtnClick} > Next &rarr;</button>
         </div>
       </div>
     );

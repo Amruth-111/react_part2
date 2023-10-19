57c5ae52a86f04379e0186da00926002626fd91d
Amruth <amruhtkrishnaa@gmail.com>
2023-10-20 00:49:17 +0530

    publishedBy and on features added

--- newsapp/src/components/NewsCompnents.js
+++ newsapp/src/components/NewsCompnents.js
@@ -29,7 +29,7 @@ export default class NewsCompnents extends Component {
   async componentDidMount() {
     this.setState({ loading: true });
     let data = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${process.env.API_KEY}&page=1&pageSize=${this.props.pageSize}`
+      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&${process.env.API_KEY}&page=1&pageSize=${this.props.pageSize}`
     );
 
     let parsedData = await data.json();
@@ -44,7 +44,7 @@ export default class NewsCompnents extends Component {
     console.log("prev click");
     this.setState({ loading: true });
     let data = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${process.env.API_KEY}&page=${
+      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&${process.env.API_KEY}&page=${
         this.state.page - 1
       }&pageSize=${this.props.pageSize}`
     );
@@ -56,13 +56,13 @@ export default class NewsCompnents extends Component {
       page: this.state.page - 1,
     });
   };
-  // ${process.env.API_KEY}
+// ${process.env.API_KEY}
   nextBtnClick = async () => {
     this.setState({ loading: true });
     console.log("nextclick");
     if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / 20))) {
       let data = await fetch(
-        `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${process.env.API_KEY}&page=${
+        `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&${process.env.API_KEY}&page=${
           this.state.page + 1
         }&pageSize=${this.props.pageSize}`
       );
@@ -94,10 +94,14 @@ export default class NewsCompnents extends Component {
                         }
                         imageURL={ele.urlToImage}
                         newsURL={ele.url}
+                        author={ele.author}
+                        date={ele.publishedAt}
+                        source={ele.source.name}
                       />
                     </div>
                   );
                 })}
+
             </div>
           </div>
         </div>
--- newsapp/src/components/NewsItemComponents.js
+++ newsapp/src/components/NewsItemComponents.js
@@ -1,25 +1,44 @@
 import React, { Component } from "react";
 
 export default class NewsItemComponents extends Component {
-  
   render() {
-    let { title, titleDescription ,imageURL,newsURL} = this.props;
+    let { title, titleDescription, imageURL, newsURL, author, date,source } =
+      this.props;
     return (
       <div>
-          <div className="card" style={{ width: "18rem" }}>
-            <img
-              src={!imageURL?"https://imgs.search.brave.com/2ZDhncsGzuIxAkSputY9UiolCx9dbSnDyshy9YVl2Tc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAxLzU3LzMzLzM1/LzM2MF9GXzE1NzMz/MzU2M190Vzl4c0ln/YnlPMHZwTk52SGVr/bFdDcDVjaGg4T0Q3/OC5qcGc":imageURL}
-              className="card-img-top"
-              alt="..."
-            />
-            <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{titleDescription}...</p>
-              <a href={newsURL} target="_blank" className="btn btn-sm btn-primary">
-               Read More
-              </a>
-            </div>
+       
+        <div className="card" style={{ width: "18rem" }}>
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{left: '90%!important', zIndex:1}}>
+          {source}
+          <span class="visually-hidden">unread messages</span>
+        </span>
+          <img
+            src={
+              !imageURL
+                ? "https://imgs.search.brave.com/2ZDhncsGzuIxAkSputY9UiolCx9dbSnDyshy9YVl2Tc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAxLzU3LzMzLzM1/LzM2MF9GXzE1NzMz/MzU2M190Vzl4c0ln/YnlPMHZwTk52SGVr/bFdDcDVjaGg4T0Q3/OC5qcGc"
+                : imageURL
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{titleDescription}...</p>
+            <p class="card-text">
+              <small class="text-body-secondary">
+                By {!author ? "Unknown" : author}on
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsURL}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
+            </a>
           </div>
+        </div>
       </div>
     );
   }
